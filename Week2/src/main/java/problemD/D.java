44b61ccc7784593586a25ef19df4a61960fdd6c9
Lars Schwegmann <schwegmannlars@gmail.com>
2019-10-28 14:17:01 +0100

    lollek

--- Week2/src/main/java/problemD/D.java
+++ Week2/src/main/java/problemD/D.java
@@ -1,7 +1,76 @@
 package problemD;
 
-public class D {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.stream.IntStream;
 
+public class D {
+
+    public static void main(String[] args) throws IOException {
+        var in = new InputStreamReader(System.in);
+        var buff = new BufferedReader(in);
+
+        var casesCount = Integer.parseInt(buff.readLine());
+
+        for (int t=1; t<=casesCount; t++) {
+            var sb = new StringBuilder("Case #");
+            sb.append(t);
+            sb.append(":\n");
+
+            var line = buff.readLine().split(" ");
+            var stationsCount = Integer.parseInt(line[0]);
+            var roomLookupCount = Integer.parseInt(line[1]);
+
+            var map = new TreeMap<Integer, Integer>();
+
+            for (int s=0; s<stationsCount; s++) {
+                // Add specified stations
+                var stationLine = buff.readLine().split(" ");
+                var stationStart = Integer.parseInt(stationLine[0]);
+                var stationEnd = Integer.parseInt(stationLine[1]);
+                IntStream.rangeClosed(stationStart, stationEnd).forEach(i -> map.put(i, map.getOrDefault(i, 0) + 1));
+            }
+
+            var lookups = new ArrayList<Integer>();
+            var lookupResults = new HashMap<Integer, Integer>();
+
+            for (int f=0; f<roomLookupCount; f++) {
+                lookups.add(Integer.parseInt(buff.readLine()));
+            }
+            var lookupsOldOrder = new ArrayList<Integer>(lookups);
+            Collections.sort(lookups);
+            var lookupIter = lookups.iterator();
+            var mapIter = map.navigableKeySet().iterator();
+            var totalIndex = 0;
+            var currentIndex = 0;
+            var current = mapIter.next();
+            var currentMax = map.get(current);
+            while (lookupIter.hasNext()) {
+                var friendIndex = lookupIter.next() - 1;
+                while (totalIndex != friendIndex) {
+                    if (currentIndex == currentMax - 1 && mapIter.hasNext()) {
+                        currentIndex = 0;
+                        current = mapIter.next();
+                        currentMax = map.get(current);
+                        totalIndex++;
+                    } else {
+                        currentIndex++;
+                        totalIndex++;
+                    }
+                }
+                lookupResults.put(friendIndex + 1, current);
+            }
+            for (var lookup : lookupsOldOrder) {
+                sb.append(lookupResults.get(lookup));
+                sb.append("\n");
+            }
+            System.out.print(sb.toString());
+            buff.readLine();
+        }
     }
 }
--- Week2/src/main/java/problemE/E.java
+++ Week2/src/main/java/problemE/E.java
@@ -1,7 +1,130 @@
 package problemE;
 
-public class E {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+public class E {
+
+    public static void main(String[] args) throws IOException {
+        var in = new InputStreamReader(System.in);
+        var buff = new BufferedReader(in);
+
+        var casesCount = Integer.parseInt(buff.readLine());
+
+        for (int t=1; t<=casesCount; t++) {
+            var nmLine = buff.readLine().split(" ");
+            var n = Integer.parseInt(nmLine[0]);
+            var m = Integer.parseInt(nmLine[1]);
+
+            // Prepare unionfind
+            var parent = new int[n];
+            var size = new int[n];
+            var hatreds = new ArrayList<HashSet<Integer>>();
+            IntStream.range(0, n).forEach(i -> {
+                parent[i] = i;
+                size[i] = 1;
+                hatreds.add(i, new HashSet<>());
+            });
+
+            for (int i=0; i<m; i++) {
+                var relationLine = buff.readLine().split(" ");
+                var type = relationLine[0].charAt(0);
+                var countryA = Integer.parseInt(relationLine[1]) - 1;
+                var countryB = Integer.parseInt(relationLine[2]) - 1;
+
+                if (type == 'F') {
+                    // Add new friend
+                    union(parent, size, hatreds, countryA, countryB);
+                } else {
+                    //hatreds.get(find(parent, countryA)).forEach(c -> union(parent, size, hatreds, countryB, c));
+                    //hatreds.get(find(parent, countryB)).forEach(c -> union(parent, size, hatreds, countryA, c));
+
+                    var a = find(parent, countryA);
+                    var b = find(parent, countryB);
+                    hatreds.get(a).add(b);
+                    hatreds.get(b).add(a);
+                }
+            }
+
+            // Compute Friends resulting from hate relationships
+            Arrays.stream(parent).distinct().forEach(p -> {
+                hatreds.get(p).forEach(h -> {
+                    hatreds.get(h).forEach(h2 -> {
+                        if (p != h2) {
+                            union(parent, size, p, h2);
+                        }
+                    });
+                });
+            });
+
+            System.out.println("Case #" + t + ": " + (size[find(parent, 0)] > n/2 ? "yes" : "no"));
+            buff.readLine();
+        }
+    }
+
+    // Unionfind calls
+
+    private static int find(int[] parent, int country) {
+        var root = country;
+        while (true) {
+            var p = parent[root];
+            if (p == root) {
+                break;
+            }
+            root = p;
+        }
+        // Compress path
+        var current = country;
+        while (current != root) {
+            var next = parent[current];
+            parent[current] = root;
+            current = next;
+        }
+
+        return root;
+    }
+
+    private static void union(int[] parent, int[] size, ArrayList<HashSet<Integer>> hatreds, int countryA, int countryB) {
+        var a = find(parent, countryA);
+        var b = find(parent, countryB);
+        if (a == b) {
+            return;
+        }
+
+        var aSize = size[a];
+        var bSize = size[b];
+
+        if (aSize < bSize) {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        parent[b] = a;
+        size[a] = aSize + bSize;
+        hatreds.get(a).addAll(hatreds.get(b));
+    }
+    private static void union(int[] parent, int[] size, int countryA, int countryB) {
+        var a = find(parent, countryA);
+        var b = find(parent, countryB);
+        if (a == b) {
+            return;
+        }
+
+        var aSize = size[a];
+        var bSize = size[b];
+
+        if (aSize < bSize) {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        parent[b] = a;
+        size[a] = aSize + bSize;
     }
 }
