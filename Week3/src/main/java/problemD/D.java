a483edceb02f8429e439ed2cf0e5ddd1e427ea33
Lars Schwegmann <schwegmannlars@gmail.com>
2019-11-04 12:29:49 +0100

    wipwipwipwipwip

--- Week3/src/main/java/problemD/D.java
+++ Week3/src/main/java/problemD/D.java
@@ -1,7 +1,52 @@
 package problemD;
 
-public class D {
-    public static void main(String[] args) throws  {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.IntStream;
 
+public class D {
+    public static void main(String[] args) throws IOException {
+        var in = new InputStreamReader(System.in);
+        var buff = new BufferedReader(in);
+
+        var casesCount = Integer.parseInt(buff.readLine());
+
+        for (int t=1; t<=casesCount; t++) {
+            var n = Integer.parseInt(buff.readLine());
+            Set<Integer>[] successors = new Set[n];
+            IntStream.range(0, n).forEach(i -> successors[i] = new HashSet<>());
+
+            for (int i=0; i<n-1; i++) {
+                var xy = Arrays.stream(buff.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                var x = xy[0];
+                var y = xy[1];
+                successors[x].add(y);
+                successors[y].add(x);
+            }
+
+            var startPath = getLongestSubtree(successors, new HashSet<>(), new Random().nextInt(n));
+            var farthest = startPath.get(startPath.size() - 1);
+            var finalPath = getLongestSubtree(successors, new HashSet<>(), farthest);
+
+            System.out.println("Case #" + t + ": " + finalPath.get(finalPath.size() / 2));
+            buff.readLine();
+        }
+    }
+
+    private static ArrayList<Integer> getLongestSubtree(Set<Integer>[] successors, HashSet<Integer> visited, int start) {
+        visited.add(start);
+        var longest = new ArrayList<Integer>();
+        for (var succ : successors[start]) {
+            if (!visited.contains(succ)) {
+                var tree = getLongestSubtree(successors, visited, succ);
+                if (tree.size() > longest.size()) {
+                    longest = tree;
+                }
+            }
+        }
+        longest.add(0, start);
+        return longest;
     }
 }
--- Week3/src/main/java/problemE/E.java
+++ Week3/src/main/java/problemE/E.java
@@ -1,7 +1,156 @@
 package problemE;
 
-public class E {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.IntStream;
 
+public class E {
+
+    public static class Edge {
+        private int source;
+        private int destination;
+        private int minWaterLevel;
+        Edge(int source, int destination, int minWaterLevel) {
+            this.source = source;
+            this.destination = destination;
+            this.minWaterLevel = minWaterLevel;
+        }
+
+        int getSource() {
+            return source;
+        }
+
+        int getDestination() {
+            return destination;
+        }
+
+        int getMinWaterLevel() {
+            return minWaterLevel;
+        }
+
+        @Override
+        public String toString() {
+            return "s: " + source + ", d: " + destination + ",l: " + minWaterLevel;
+        }
+    }
+
+    public static class ControlRoom {
+        private int room;
+        private int level;
+        ControlRoom(int room, int level) {
+            this.room = room;
+            this.level = level;
+        }
+        int getRoom() {
+            return room;
+        }
+        int getLevel() {
+            return level;
+        }
+
+        @Override
+        public String toString() {
+            return "r" + room + ", l: " + level;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        var in = new InputStreamReader(System.in);
+        var buff = new BufferedReader(in);
+
+        var casesCount = Integer.parseInt(buff.readLine());
+
+        for (int t=1; t<=casesCount; t++) {
+            var nmkl = Arrays.stream(buff.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            var n = nmkl[0]; // Number of rooms numbered [1; n]
+            var m = nmkl[1]; // Number of hallways
+            var k = nmkl[2]; // Number of control rooms
+            var l = nmkl[3]; // Start water level
+
+            ArrayList<Edge>[] hallways = new ArrayList[n];
+            IntStream.range(0, n).forEach(i -> hallways[i] = new ArrayList<>());
+            HashMap<Integer, Integer> controlRooms = new HashMap<>();
+
+            for (int i=0; i<m; i++) {
+                var abl = Arrays.stream(buff.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                var ai = abl[0] - 1;
+                var bi = abl[1] - 1;
+                var li = abl[2];
+                hallways[ai].add(new Edge(ai, bi, li));
+                hallways[bi].add(new Edge(bi, ai, li));
+            }
+
+            for (int i=0; i<k; i++) {
+                var ad = Arrays.stream(buff.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+                var a = ad[0] - 1;
+                var d = ad[1];
+                controlRooms.put(a, d);
+            }
+
+            var currentWaterLevel = l;
+            var reachableControlRooms = new TreeSet<>(Comparator.comparing(ControlRoom::getLevel));
+            var visited = new HashSet<Integer>();
+            var queue = new PriorityQueue<>(Comparator.comparing(Edge::getMinWaterLevel).reversed());
+
+            hallways[0].forEach(e -> {
+                if (controlRooms.containsKey(e.getDestination())) {
+                    reachableControlRooms.add(new ControlRoom(e.getDestination(), controlRooms.get(e.getDestination())));
+                }
+                queue.add(e);
+            });
+            visited.add(0);
+
+            while (!queue.isEmpty() && visited.size() < n) {
+                var candidate = queue.peek();
+                if (visited.contains(candidate.getDestination())) {
+                    queue.poll();
+                    continue;
+                }
+                if (candidate.minWaterLevel >= currentWaterLevel) {
+                    // Water level ist still sufficient
+                    queue.poll(); // Remove candidate
+                    visited.add(candidate.getDestination()); // Add candidate to visited set
+                    if (controlRooms.containsKey(candidate.getDestination())) {
+                        // Add control room to available control rooms
+                        var c = new ControlRoom(candidate.getDestination(), controlRooms.get(candidate.getDestination()));
+                        reachableControlRooms.add(c);
+                    }
+                    // Remove edges leading to this
+                    queue.removeIf(e -> e.getDestination() == candidate.getDestination());
+                    // Add edges from destination of candidates to queue
+                    hallways[candidate.getDestination()]
+                            .stream()
+                            .filter(e -> !visited.contains(e.getDestination()))
+                            .forEach(queue::add);
+                } else {
+                    // Try lowering the water level
+                    if (reachableControlRooms.size() == 0) {
+                        // No control room reachable, cant lower water any further, stop
+                        break;
+                    }
+
+                    var minimumWaterLevel = candidate.getMinWaterLevel();
+                    var mostPowerfulControlRoom = reachableControlRooms.first();
+
+                    if (minimumWaterLevel < mostPowerfulControlRoom.getLevel()) {
+                        // water level is not achievable with available control rooms, stop
+                        break;
+                    }
+
+                    // this minimum water level is achievable, set current to required minimum
+                    currentWaterLevel = minimumWaterLevel;
+                    if (minimumWaterLevel - mostPowerfulControlRoom.getLevel() == 0) {
+                        // Remove control room from reachable rooms as its no longer useful
+                        reachableControlRooms.remove(mostPowerfulControlRoom);
+                    }
+                }
+            }
+
+            System.out.println("Case #" + t + ": " + (visited.size() == n ? currentWaterLevel : "impossible"));
+
+            buff.readLine();
+        }
     }
 }
